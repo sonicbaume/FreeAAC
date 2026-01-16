771427fd769251dfb916c6132fc314210e76666d
Chris Baume <chris@bau.me>
2026-01-16 09:47:50 +0000

    Load file using AAC processors

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,18 @@
+import { GridsetProcessor } from '@willwade/aac-processors/browser';
 import * as DocumentPicker from "expo-document-picker";
 import { File } from "expo-file-system";
 import { Button, View } from "react-native";
-j
+
 export default function Index() {
   const handleOpenFile = async () => {
     const result = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: true })
     const fileName = result.assets?.at(0)
     if (!fileName) return console.debug("No files selected")
     const file = new File(fileName.uri)
-    console.log(file.textSync())
+    const buffer = await file.arrayBuffer()
+    const processor = new GridsetProcessor();
+    const tree = await processor.loadIntoTree(buffer);
+    console.log(tree)
   }
   return (
     <View
