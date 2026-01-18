083a1ff38c333f33a1622ecb1f4edc5b3dc0cfe9
Chris Baume <chris@bau.me>
2026-01-18 10:06:29 +0000

    Added basic TTS

--- app/components/Tile.tsx
+++ app/components/Tile.tsx
@@ -1,13 +1,21 @@
 import { AACButton } from "@willwade/aac-processors/browser"
 import { Pressable, StyleSheet, Text } from "react-native"
+import { speak } from "../utils/speech"
 
 export default function Tile({
   button,
 }: {
   button: AACButton
 }) {
+
+  const handlePress = () => {
+    if (button.action?.type === "SPEAK") {
+      speak(button.action.message ?? button.message)
+    }
+  }
+
   return (
-    <Pressable style={styles.container}>
+    <Pressable style={styles.container} onPress={handlePress}>
       <Text style={styles.label}>{button.label}</Text>
     </Pressable>
   )
