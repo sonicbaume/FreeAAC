551a6a7af8554cddfe33a5e6546a4897272e95d6
Chris Baume <chris@bau.me>
2026-01-18 23:31:15 +0000

    Added persistant storage

--- app/stores/prefs.ts
+++ app/stores/prefs.ts
@@ -1,14 +1,22 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { zustandStorage } from './middleware';
 
 interface PrefsState {
   playOnPress: boolean;
   togglePlayOnPress: () => void;
 }
 
-const usePrefsStore = create<PrefsState>((set) => ({
-  playOnPress: true,
-  togglePlayOnPress: () => set((state) => ({ playOnPress: !state.playOnPress })),
-}))
+export const usePrefsStore = create<PrefsState>()(persist(
+  (set, get) => ({
+    playOnPress: true,
+    togglePlayOnPress: () => set({ playOnPress: !get().playOnPress }),
+  }),
+  {
+    name: 'prefs',
+    storage: createJSONStorage(() => zustandStorage)
+  },
+))
 
 export const usePlayOnPress = () => usePrefsStore(s => s.playOnPress)
 export const useTogglePlayOnPress = () => usePrefsStore(s => s.togglePlayOnPress)
