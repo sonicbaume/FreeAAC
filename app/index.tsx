38e6b81b7a0c06eae5db335444a3097bc0327cd5
Chris Baume <chris@bau.me>
2026-01-20 11:25:10 +0000

    Added board list

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { useRouter } from "expo-router";
-import { Button, View } from "react-native";
-import { usePagesetActions } from "./stores/pagesets";
+import { Button, FlatList, Text, View } from "react-native";
+import { usePagesetActions, useTreeFiles } from "./stores/pagesets";
 import { handleError } from "./utils/error";
 import { selectFile } from "./utils/file";
 
 export default function Index() {
   const router = useRouter()
+  const treeFiles = useTreeFiles()
   const { addTreeFile } = usePagesetActions()
 
   const handleOpenFile = async () => {
@@ -19,8 +20,22 @@ export default function Index() {
   }
 
   return <>
-    <View style={{ flex: 1 }}>
-      <Button title="Import board" onPress={handleOpenFile}/>
+    <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
+      <View style={{ width: 400, maxWidth: '100%', padding: 20, gap: 20 }}>
+        <Text style={{ fontSize: 18 }}>My boards</Text>
+        <FlatList
+          data={treeFiles}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+          <Button
+            title={item}
+            onPress={() => {
+              router.push({ pathname: '/[board]', params: { board: item } })
+            }}
+          />
+        )}/>
+        <Button title="Import board" onPress={handleOpenFile}/>
+      </View>
     </View>
   </>
 }
