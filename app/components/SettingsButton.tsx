6f6cf965c1ed93a6de400b7a7acf3f6a71307738
Chris Baume <chris@bau.me>
2026-01-22 15:44:08 +0000

    Changed settings icon

--- app/components/SettingsButton.tsx
+++ app/components/SettingsButton.tsx
@@ -1,12 +1,12 @@
 import { Link } from "expo-router";
-import { Menu } from "lucide-react-native";
+import { Settings } from "lucide-react-native";
 import { Pressable } from "react-native";
 
 export default function SettingsButton() {
   return (
     <Link href="/settings" asChild>
       <Pressable style={{ padding: 10 }}>
-        <Menu size={24} color="black" />
+        <Settings size={24} color="black" />
       </Pressable>
     </Link>
   )
