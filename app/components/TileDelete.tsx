abfb8bff6a3373678925c9a0368f0b48720baafd
Chris Baume <chris@bau.me>
2026-02-27 13:27:57 +0000

    Use DialogConfirm for tile deletion

--- app/components/TileDelete.tsx
+++ app/components/TileDelete.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import { GAP, useTheme } from "../utils/theme";
+import { useTheme } from "../utils/theme";
+import DialogConfirm from "./DialogConfirm";
 import { Button, Text } from "./Styled";
 
 export default function TileDelete ({
@@ -9,47 +9,23 @@ export default function TileDelete ({
   onPress: () => void;
 }) {
   const theme = useTheme()
-  const [showConfirm, setShowConfirm] = useState(false)
-  
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false)
   return <>
-    {!showConfirm &&
     <Button
       variant="destructive"
-      onPress={() => setShowConfirm(true)}
+      onPress={() => setShowConfirmDialog(true)}
     >
       <Text style={{ color: theme.onError }}>Delete</Text>
     </Button>
-    }
-    {showConfirm &&
-    <View style={styles.confirmContainer}>
-      <Text>
-        Are you sure you want to delete?
-      </Text>
-      <View style={styles.confirmButtonContainer}>
-        <Button variant="outline" style={{ flex: 1 }} onPress={() => setShowConfirm(false)}>
-          <Text style={{ color: theme.onSurface }}>No</Text>
-        </Button>
-        <Button variant="destructive" style={{ flex: 1 }} onPress={onPress}>
-          <Text style={{ color: theme.onError }}>Yes</Text>
-      </Button>
-      </View>
-    </View>
+    {showConfirmDialog &&
+    <DialogConfirm
+      visible={showConfirmDialog}
+      onCancel={() => setShowConfirmDialog(false)}
+      onConfirm={() => {
+        onPress()
+        setShowConfirmDialog(false)
+      }}
+    />
     }
   </>
 }
-
-const styles = StyleSheet.create({
-  confirmButtonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    maxWidth: '50%',
-    gap: GAP.lg
-  },
-  confirmContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: GAP.lg,
-    justifyContent: 'space-between',
-  },
-})
