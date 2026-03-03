3c3b07be1f10964581aeb0c688f8b0d00566a461
Chris Baume <chris@bau.me>
2026-03-03 15:10:15 +0000

    Remove disabled option (unsupported in OBF)

--- app/utils/consts.ts
+++ app/utils/consts.ts
@@ -263,10 +263,9 @@ export const colValues = [
 ]
 export const defaultColValue = '6'
 
-//TODO Add support for Empty and PointerAndTouchOnly
-export type ButtonVisibility = 'Visible' | 'Hidden' | 'Disabled'
+//TODO Add support for Disabled, Empty and PointerAndTouchOnly
+export type ButtonVisibility = 'Visible' | 'Hidden'
 export const buttonVisibilityValues: { value: ButtonVisibility, label: string}[] = [
   { value: "Visible", label: "Visible" },
   { value: "Hidden", label: "Hidden" },
-  { value: "Disabled", label: "Disabled" },
 ]
