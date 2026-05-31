f0cda59c43f1a2b2b302fef8e82151d7d5b61ee7
Chris Baume <chris@bau.me>
2026-05-31 11:37:47 +0100

    Removed verbose comment

--- components/Board/BoardOptions.tsx
+++ components/Board/BoardOptions.tsx
@@ -49,7 +49,7 @@ export default function BoardOptions({
     if (!board) return handleError("No board found")
     await deleteBoard(board.id)
     removeBoard(board.id)
-    if (isDefaultBoard) setDefaultBoardId(undefined) // <-- adding default board id clearing to prevent redirecting to deleted board
+    if (isDefaultBoard) setDefaultBoardId(undefined)
     setShowDeleteDialog(false)
     ref.current?.dismiss()
   }
